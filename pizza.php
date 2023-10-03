dd7405bcc94b4fde73c62cdb98182c046442cb30
alpima <15586179.clot@fje.edu>
2023-10-03 19:28:43 +0200

    Commit 1 del projecte pizza

---
+++ pizza.php
@@ -0,0 +1,3 @@
+
+<!-- saved from url=(0054)https://www.collados.org/asix2/m14/GP1/pizza/pizza.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body>Has demanat només la pizza b�&nbsp;sica. El preu és 9.68 € (IVA inclòs)<br></body></html>
